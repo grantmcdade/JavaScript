043efc7585e2ab0c39e2ef9b246b5a84d33880c0
Grant McDade <grant.mcdade@gmail.com>
2017-01-31 12:55:32 +0100

    Competed day 10's challenge

--- equal.ts
+++ equal.ts
@@ -22,17 +22,59 @@ export class EqualSolution {
     public solve(input: string) {
         let lines = input.split('\n');
         let testCount = parseInt(lines[0]);
-        for (let test = 1; test <= testCount; test += 2) {
-            let coInterCount = parseInt(lines[test]);
-            let chocolateCount = lines[test + 1].split(' ');
-            chocolateCount.map(Number);
+        let currentLine = 1;
+        let options = [1, 2, 5];
+        for (let test = 1; test <= testCount; test++) {
+            let coInterCount = parseInt(lines[currentLine++]);
+            let chocolateCount = lines[currentLine++].split(' ').map(Number);
+
+            let minOperations: number[][][] = [];
+            for (let k = 0; k < coInterCount; k++) {
+                let internChocolateCount: number = <any>chocolateCount[k] as number;
+                if (minOperations[k] === undefined) {
+                    minOperations[k] = [];
+                }
+                for (let m = 0; m <= internChocolateCount; m++) {
+                    if (minOperations[k][m] === undefined) {
+                        minOperations[k][m] = [];
+                    }
+                    for (var n = 0; n < options.length; n++) {
+                        if (minOperations[k][m][n] === undefined) {
+                            minOperations[k][m][n] = Number.MAX_SAFE_INTEGER;
+                        }
+                    }
+                }
+            }
 
             // Find the least number of operations
-            let minOperations: number[][] = [];
             for (let intern = 0; intern < coInterCount; intern++) {
-                let currentChocolateCount = chocolateCount[intern];
+                let internChocolateCount: number = <any>chocolateCount[intern] as number;
+                let internTable = minOperations[intern];
+                for (let chocolateCount = 1; chocolateCount <= internChocolateCount; chocolateCount++) {
+                    for (let optionIndex = 0; optionIndex < options.length; optionIndex++) {
 
+                        let option = options[optionIndex];
+                        if (option === chocolateCount) {
+                            internTable[chocolateCount][option] = Math.min(internTable[chocolateCount - 1][option] + 1, chocolateCount / option);
+                        }
+
+                        // if (chocolateCount < options[optionIndex]) {
+                        //     minOperations[intern][chocolateCount][optionIndex] = minOperations[intern][chocolateCount - 1][options[optionIndex]];
+                        // } else if (chocolateCount === options[optionIndex]) {
+                        //     minOperations[intern][chocolateCount][optionIndex] = 1;
+                        // }
+                        // else if (chocolateCount % options[optionIndex] === 0) {
+                        //     minOperations[intern][chocolateCount][optionIndex] = Math.min(minOperations[intern][chocolateCount - 1][options[optionIndex]] + 1, chocolateCount / options[optionIndex]);
+                        // }
+                        // else {
+                        //     minOperations[intern][chocolateCount][optionIndex] = minOperations[intern][chocolateCount - 1][optionIndex] + 1;
+                        // }
+
+                    }
+                }
             }
+
+            console.log(minOperations);
         }
     }
 }
